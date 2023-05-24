eb631fd1ce74d62f881eb796cb7c25a5995a7b5f
Zach <104291766+zachary-o@users.noreply.github.com>
2023-05-24 20:04:11 +0300

    add login function

--- src/components/Authorization.jsx
+++ src/components/Authorization.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 
 import getUsers from "../utils/getUsers";
+import registerUser from "../utils/registerUser";
 
 const Authorization = () => {
   const [changeAuth, setChangeAuth] = useState(true);
   const [users, setUsers] = useState([]);
+  const [formData, setFormData] = useState({
+    login: "",
+    password: "",
+  });
+  const [authStatus, setAuthStatus] = useState("");
 
   const handleChangeAuth = () => {
     setChangeAuth(!changeAuth);
@@ -19,6 +25,29 @@ const Authorization = () => {
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  console.log(formData);
+
+  const validateFormData = (event) => {
+    event.preventDefault();
+    if (changeAuth) {
+      if (formData.login === "" || formData.password === "") {
+        setAuthStatus("Please enter credentials");
+      }
+      if (
+        users.find((user) => user.login === formData.login) &&
+        users.find((user) => user.password === formData.password)
+      ) {
+        setAuthStatus("Login success");
+      }
+      if (
+        users.find((user) => user.login !== formData.login) ||
+        users.find((user) => user.password !== formData.password)
+      ) {
+        setAuthStatus("Incorrect login or password");
+      }
+    }
+  };
 
   return (
     <div className="auth-wrapper">
@@ -40,9 +69,30 @@ const Authorization = () => {
       </div>
 
       <form action="">
-        <input type="text" required />
-        <input type="password" required />
-        <button>{changeAuth ? "Login" : "Register"}</button>
+        <input
+          type="text"
+          name="login"
+          placeholder="Login"
+          required
+          value={formData.login}
+          onChange={(event) =>
+            setFormData({ ...formData, login: event.target.value })
+          }
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          value={formData.password}
+          onChange={(event) =>
+            setFormData({ ...formData, password: event.target.value })
+          }
+        />
+        <h4>{authStatus}</h4>
+        <button onClick={(event) => validateFormData(event)}>
+          {changeAuth ? "Login" : "Register"}
+        </button>
       </form>
     </div>
   );
--- src/utils/registerUser.js
+++ src/utils/registerUser.js
@@ -1,1 +1,12 @@
 import axios from "axios";
+
+const registerUser = async (user) => {
+  const response = await axios.post(
+    "https://646d10197b42c06c3b2c7fc2.mockapi.io/users", user
+  );
+
+  const data = response.data;
+  return data;
+};
+
+export default registerUser
